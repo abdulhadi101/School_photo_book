9e29c10cb9e5bc37012df8615719dd598542b051
abdulhadi101 <get2abdul101@yahoo.com>
2019-05-15 10:32:09 +0100

    initial commit

---
+++ app/src/main/java/com/example/school_photo_book/book_mode.java
@@ -0,0 +1,14 @@
+package com.example.school_photo_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class book_mode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_mode);
+    }
+}
