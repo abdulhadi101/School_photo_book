09a63de6c290e5ad7440d508dbd61b9afc0614e6
abdulhadi101 <get2abdul101@yahoo.com>
2019-06-16 09:12:17 +0100

    other activities added

---
+++ app/src/main/java/com/example/school_photo_book/ClassC.java
@@ -0,0 +1,14 @@
+package com.example.school_photo_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClassC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_c);
+    }
+}
