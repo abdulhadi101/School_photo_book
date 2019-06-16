497caa6a7dfbc615282cb0c062ddc7aabeeed49d
abdulhadi101 <get2abdul101@yahoo.com>
2019-06-16 11:25:10 +0100

    categories UI 50%

--- app/src/main/java/com/example/school_photo_book/ClassC.java
+++ app/src/main/java/com/example/school_photo_book/ClassC.java
@@ -1,14 +1,100 @@
 package com.example.school_photo_book;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ClassC extends AppCompatActivity {
+
+    int currentPage = 0;
+    int NUM_PAGES = 10;
+    Timer timer;
+    final long DELAY_MS = 500;//delay in milliseconds before task is to be executed
+    final long PERIOD_MS = 3000; // time in milliseconds between successive task executions.
+
+
+
+    // Declare Variables
+    ViewPager viewPager;
+    PagerAdapter adapter;
+    String[] Name;
+    String[] Nickname;
+    String[] Phonenumber;
+    String[] Address;
+    String[] Hobby;
+    String[] Ambition;
+    String[] Comment;
+    int[] photo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_c);
+
+        // Generate sample data
+        Name = new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+
+        Nickname = new String[] { "China", "India", "United States",
+                "Indonesia", "Brazil", "Pakistan", "Nigeria", "Bangladesh",
+                "Russia", "Japan" };
+        Phonenumber = new String[] { "China", "India", "United States",
+                "Indonesia", "Brazil", "Pakistan", "Nigeria", "Bangladesh",
+                "Russia", "Japan" };
+        Address = new String[] { "1,354,040,000", "1,210,193,422",
+                "315,761,000", "237,641,326", "193,946,886", "182,912,000",
+                "170,901,000", "152,518,015", "143,369,806", "127,360,000" };
+        Hobby = new String[] { "1,354,040,000", "1,210,193,422",
+                "315,761,000", "237,641,326", "193,946,886", "182,912,000",
+                "170,901,000", "152,518,015", "143,369,806", "127,360,000" };
+        Ambition = new String[] { "1,354,040,000", "1,210,193,422",
+                "315,761,000", "237,641,326", "193,946,886", "182,912,000",
+                "170,901,000", "152,518,015", "143,369,806", "127,360,000" };
+        Comment = new String[] { "1,354,040,000", "1,210,193,422",
+                "315,761,000", "237,641,326", "193,946,886", "182,912,000",
+                "170,901,000", "152,518,015", "143,369,806", "127,360,000" };
+
+
+
+
+        photo = new int[] { R.drawable.abdul, R.drawable.abdul1,
+                R.drawable.abdul, R.drawable.abdul1,
+                R.drawable.abdul, R.drawable.abdul1, R.drawable.abdul,
+                R.drawable.abdul1, R.drawable.abdul, R.drawable.abdul1 };
+
+        // Locate the ViewPager in viewpager_main.xml
+        viewPager = findViewById(R.id.pagerc);
+        // Pass results to ViewPagerAdapter Class
+        adapter = new ViewPagerAdapter(ClassC.this, Name, Nickname, Phonenumber, Address, Hobby, Ambition, Comment, photo);
+        // Binds the Adapter to the ViewPager
+        viewPager.setAdapter(adapter);
+
+
+        /*After setting the adapter use the timer */
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == NUM_PAGES-1) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        timer = new Timer(); // This will create a new Thread
+        timer.schedule(new TimerTask() { // task to be scheduled
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, DELAY_MS, PERIOD_MS);
+
+
     }
 }
--- app/src/main/java/com/example/school_photo_book/MainActivity.java
+++ app/src/main/java/com/example/school_photo_book/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.school_photo_book;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button categories_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        categories_btn = findViewById(R.id.categories_btn);
+
+        categories_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Categories.class);
+                startActivity(intent);
+            }
+        });
     }
 }
